a3d52f1f38dcb3db6d6da06f4e78ed8e27ae98d8
kevkatam <kevkatam@gmail.com>
2023-10-10 12:38:46 -0700

    added test_state.py

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -17,3 +17,21 @@ class TestUser(unittest.TestCase):
     def test_inheritance(self):
         """ tests if class User inherits or is subclass of class BaseModel """
         self.assertIsInstance(self.u, User)
+
+    def test_has_attributes(self):
+        """ tests if all attributes exit """
+        self.assertTrue(hasattr(self.u, 'email'))
+        self.assertTrue(hasattr(self.u, 'first_name'))
+        self.assertTrue(hasattr(self.u, 'last_name'))
+        self.assertTrue(hasattr(self.u, 'password'))
+
+    def test_types(self):
+        """ test if attribute type is correct """
+        self.assertIsInstance(self.u.email, str)
+        self.assertIsInstance(self.u.first_name, str)
+        self.assertIsInstance(self.u.last_name, str)
+        self.assertIsInstance(self.u.password, str)
+
+
+if __name__ == '__main__':
+    unittest.main()
