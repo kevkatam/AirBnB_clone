d6896102884f3ae2809b136c50d36bdcde4e977c
kevkatam <kevkatam@gmail.com>
2023-10-10 05:05:41 -0700

    modified console.py

--- console.py
+++ console.py
@@ -3,11 +3,15 @@
 entry point of the command interpreter
 """
 import cmd
+"""from models.base_model import BaseModel
+from models import storage"""
 
 
 class HBNBCommand(cmd.Cmd):
     """ Command interpreter """
     prompt = "(hbnb) "
+    class_list = ['BaseModel']
+    command_list = ['create']
 
     def help_help(self):
         """ prints help command description """
@@ -16,6 +20,64 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ executes nothing when emptyline is given """
         pass
+
+    def do_create(self, model):
+        """ Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id """
+        if not model:
+            print("** class name missing **")
+        elif model not in HBNBCommand.class_list:
+            print("** class doesn't exist **")
+        else:
+            dct = {'BaseModel': BaseModel}
+            mymodel = dct[model]()
+            print(mymodel.id)
+            mymodel.save()
+
+    def do_show(self, arg):
+        """ Prints the string representation of an instance based on the class
+        name and id """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split(' ')
+
+        if args[0] not in HBNBCommand.class_list:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+            all_objs = storage.all()
+            for key, value in all_objs.items():
+                obj_name = value.__class__.__name__
+                obj_id = value.id
+                if obj_name == args[0] and obj_id == args[1].strip('"'):
+                    print(value)
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """  Deletes an instance based on the class name and id
+        (save the change into the JSON file) """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split(' ')
+
+        if args[0] not in HBNBCommand.class_list:
+            print("** class doesn't exist **")
+        
+        elif len(args) == 1:
+            print("** instance id missing **")
+            all_objs = storage.all()
+            for key, value in all_objs.items():
+                obj_name = value.__class__.__name__
+                obj_id = value.id
+                if obj_name == args[0] and obj_id == args[1].strip('"'):
+                    del value
+                    del storage._FileStorage__objects[key]
+                    storage.save()
+                    return
+            print("** no instance found **")
 
     def do_quit(self, line):
         """ Quit command to exit the program """
