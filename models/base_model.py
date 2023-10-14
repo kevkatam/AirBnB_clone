c4ab851d82e92dc873984525dae2f7cab2d83601
kevkatam <kevkatam@gmail.com>
2023-10-14 07:17:59 -0700

    modified base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -27,9 +27,7 @@ class BaseModel:
 
     def __str__(self):
         '''String representation'''
-        d = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
-
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, d))
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         '''Update current time'''
