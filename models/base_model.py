51617a882254e5d871758d5cb79ff9d2bed80042
kevkatam <kevkatam@gmail.com>
2023-10-14 06:39:55 -0700

    modified base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
                     continue
                 if key == 'created_at' or key == 'updated_at':
                     v = datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, v)
+                    setattr(self, key, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
